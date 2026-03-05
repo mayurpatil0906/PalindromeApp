c5b07046945c1db6ce89ec4e5d1bb97b12721f68
mayurpatil0906 <146657995+mayurpatil0906@users.noreply.github.com>
2026-03-05 10:38:23 +0530

    Update UCsix.java

--- PalindromeChecker/src/com/palindromeChecker/UCsix.java
+++ PalindromeChecker/src/com/palindromeChecker/UCsix.java
@@ -3,25 +3,25 @@ import java.util.*;
 public class UCsix {
 	public static void main(String[] args) {
 		String s="naman";
-		Stack<Character> stack=new Stack<>();
-		Queue<Character> queue=new LinkedList<>();
+		Stack<Character> stack=new Stack<>();//create Stack
+		Queue<Character> queue=new LinkedList<>();//create Queue
 		for (int i=0;i<s.length();i++) {
 			char ch = s.charAt(i);
-			stack.push(ch);
-			queue.add(ch);
+			stack.push(ch);//pushed element
+			queue.add(ch);//add element
 		}
 		boolean isPalindrome=true;
 		for(int i=0;i<s.length();i++) {
-			char poppedfromQueue=queue.poll();
-			char poppedfromStack=stack.pop();
-			if (poppedfromQueue!=poppedfromStack) {
+			char poppedfromQueue=queue.poll();//poped element from queue
+			char poppedfromStack=stack.pop();//popped element from Stack
+			if (poppedfromQueue!=poppedfromStack) { //compare popped elements
 				isPalindrome=false;
 				break;
 			}
 		}
 		System.out.println("Input String: " + s);
 
-        if (isPalindrome) {
+        if (isPalindrome) {//check
             System.out.println("Result: It is a Palindrome ");
         } else {
             System.out.println("Result: It is NOT a Palindrome ");
