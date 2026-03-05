c468ae2c295c865a6deea3cc4e9cdcef63a4e068
mayurpatil0906 <mayurpatill0906@gmail.com>
2026-03-05 10:03:30 +0530

    Coded UC7 to check palindrome using dequeue

---
+++ PalindromeChecker/src/com/palindromeChecker/UCseven.java
@@ -0,0 +1,33 @@
+package com.palindromeChecker;
+import java.util.Deque;
+import java.util.ArrayDeque;
+public class UCseven {
+	public static void main(String[] args) {
+		String input = "radar";
+		// Create Deque
+		Deque<Character> deque = new ArrayDeque<>();
+		// Step 3: Insert characters into deque
+		for (int i = 0; i < input.length(); i++) {
+			deque.addLast(input.charAt(i));
+		}
+		boolean isPalindrome = true;
+		// Compare front and rear elements
+		while (deque.size() > 1) {
+			char front = deque.removeFirst(); // remove from front
+			char rear = deque.removeLast();   // remove from rear
+			if (front != rear) {
+				isPalindrome = false;
+				break;
+			}
+		}
+		// Display result
+		System.out.println("Input String: " + input);
+		if (isPalindrome) {
+			System.out.println("Result: It is a Palindrome ");
+		} else {
+			System.out.println("Result: It is NOT a Palindrome ");
+		}
+	}
+}
+
+
